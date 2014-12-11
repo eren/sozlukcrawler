64b4cdb4436bdbd359f970e57435568281dba5f3
Eren Türkay <turkay.eren@gmail.com>
2014-12-11 16:07:05 +0200

    pipeline: DatabasePipeline ekle
    Parse edilen her girdiyi settings.py dosyasinda belirtilen veritabanina yaz

--- sozlukcrawl/pipelines.py
+++ sozlukcrawl/pipelines.py
@@ -1,11 +1,37 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy import log
+from .models import session, Girdi, Engine, create_tables
 
 
-class SozlukscrapyPipeline(object):
+class DatabasePipeline(object):
+    def __init__(self):
+        create_tables(Engine)
+    
     def process_item(self, item, spider):
+        """
+        Scrape edilen her girdiyi veritabanina ekle. Bu method sayfa process edildikten, icerisindeki
+        bilgiler cekildikten ve Item objesi olusturulduktan sonra her seferinde cagriliyor.
+
+        :param item: Parse edilmis nesne
+        :type item: Scrapy item
+        :param spider: Su anda calisan, spiders/ dizini altinda belirtilen spiderlardan herhangi biri
+        :type spider: Scrapy spider
+        :return: Gonderilen Item
+        :rtype: Scrapy item
+        """
+        log.msg('[%s] PROCESSING ITEM [item no: %s, baslik: %s]' %
+                (spider.name, item['girdi_id'], item['baslik']),
+                level=log.INFO)
+
+        girdi = Girdi(**item)
+        try:
+            session.add(girdi)
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
         return item

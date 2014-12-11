06b772a0014f9cf4915dd0111e2091dceb2aba77
Eren Türkay <turkay.eren@gmail.com>
2014-12-11 16:05:52 +0200

    items.py: source ve baslik alanlarini ekle

--- sozlukcrawl/items.py
+++ sozlukcrawl/items.py
@@ -9,8 +9,10 @@ import scrapy
 
 
 class Girdi(scrapy.Item):
+    source = scrapy.Field()
     girdi_id = scrapy.Field()
     baslik_id = scrapy.Field()
+    baslik = scrapy.Field()
     text = scrapy.Field()
     date = scrapy.Field()
     time = scrapy.Field()

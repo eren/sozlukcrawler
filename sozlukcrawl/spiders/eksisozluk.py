56fae31487aa62d594a24c1749dc72532011a304
Eren Türkay <turkay.eren@gmail.com>
2014-12-11 16:07:36 +0200

    spiders/eksisozluk: baslik bilgisini al, tarih bilgisini ters cevir
    - Veritabani GG.AA.YYYY yerine YYYY.AA.GG beklemekte. Tarih bilgisini ters cevir
    - Baslik bilgisi icin XPath yaz

--- sozlukcrawl/spiders/eksisozluk.py
+++ sozlukcrawl/spiders/eksisozluk.py
@@ -3,35 +3,46 @@
 from scrapy import Spider
 from scrapy import log
 from scrapy.http import Request
+from scrapy.exceptions import CloseSpider
 
 from ..items import Girdi
 
 
 class EksisozlukBaslikSpider(Spider):
-    name = "eksibaslik"
-    
+    name = "eksisozluk"
+
     def __init__(self, **kwargs):
         super(EksisozlukBaslikSpider, self).__init__(**kwargs)
 
-        assert('urls' in kwargs)
+        if 'urls' not in kwargs:
+            raise CloseSpider('URL should be given to scrape')
+
         self.start_urls = kwargs['urls'].split(',')
         self.allowed_domains = ["eksisozluk.com"]
 
     def parse(self, response):
-        log.msg('PARSING', log.DEBUG)
-
+        self.log("PARSING: %s" % response.request.url, level=log.INFO)
         for sel in response.xpath('//*[@id="entry-list"]/li/article'):
             girdi_id = sel.xpath('./footer/@data-id').extract()[0]
             baslik_id = response.xpath('//*[@id="title"]/a/@href').re(r'--(\d*)')[0]
+            baslik = response.xpath('//*[@id="title"]/a/span/text()').extract()[0]
             date = sel.xpath('./footer/div[2]/span/time/text()').re(r'\d{2}[.]\d{2}[.]\d{4}')[0]
             time = sel.xpath('./footer/div[2]/span/time/text()').re(r'\d{2}[:]\d{2}')[0]
             text = sel.xpath('string(./div)').extract()[0]
             nick = sel.xpath('./footer/div[2]/address/a/span/text()').extract()[0]
 
-            item = Girdi()#
+            item = Girdi()
+            item['source'] = self.name
+            item['baslik'] = baslik
             item['girdi_id'] = girdi_id
             item['baslik_id'] = baslik_id
-            item['date'] = date
+
+            # GG.AA.YYYY formatini YYYY.AA.GG formatina cevir. Veritabani bu formatta bekliyor.
+            # DateTime modulu kullanilarak da yapilabilir ama ugrasmayalim simdi.
+            reverse_date = date.split('.')
+            reverse_date.reverse()
+            item['date'] = '.'.join(reverse_date)
+
             item['time'] = time
             item['text'] = text
             item['nick'] = nick
@@ -47,6 +58,6 @@ class EksisozlukBaslikSpider(Spider):
         current_url = response.request.url.split('?p')[0]
 
         next_page = current_page + 1
-        # if page_count >= next_page:
-        if current_page < 3:
+        if page_count >= next_page:
+        # if current_page < 1:
             yield Request('%s?p=%s' % (current_url, next_page))

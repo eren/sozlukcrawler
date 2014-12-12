5302035abf33cd97ad9e0d7ee3c84f8cc0bc31b7
Eren Türkay <turkay.eren@gmail.com>
2014-12-12 09:36:40 +0200

    spiders/__init__: GenericSozlukSpider ekle
    Her sozlukte scrape etmeden once bazi kontrolleri yapmamiz gerekiyor. Bu
    kontrolleri her seferinde kopyala/yapistir yapmak yerine tek bir yerde topla.

--- sozlukcrawl/spiders/__init__.py
+++ sozlukcrawl/spiders/__init__.py
@@ -2,3 +2,36 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+
+from scrapy import Spider
+from scrapy import log
+from scrapy.http import Request
+from scrapy.exceptions import CloseSpider
+
+from ..utils import is_request_seen
+
+
+class GenericSozlukSpider(Spider):
+    """
+    Her sozluk spider'inda ortak olan isleri burada topla.
+
+    Yeni bir sozluk eklenmesi gerektigi zaman bu class'in genisletilip parse() methodunun
+    yazilmasi yeterli.
+    """
+    def __init__(self, **kwargs):
+        super(GenericSozlukSpider, self).__init__(**kwargs)
+
+        if 'urls' not in kwargs:
+            raise CloseSpider('URL should be given to scrape')
+
+        self.urls = kwargs['urls'].split(',')
+        self.allowed_domains = []
+
+    def start_requests(self):
+        self.log('Eliminating already seen web pages. If you think crawler is not working '
+                 'please check "seen" table in the database', level=log.WARNING)
+
+        return [Request(i) for i in self.urls if not is_request_seen(Request(i))]
+
+    def parse(self, response):
+        raise NotImplementedError
